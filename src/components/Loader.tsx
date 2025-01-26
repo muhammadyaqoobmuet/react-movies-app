c6d3b0aed521df61b67397f31db8e846e3d01547
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-26 19:20:09 +0500

    add Loader component to display loading state for movie cards

---
+++ src/components/Loader.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+
+function Loader() {
+
+  return (
+    <div className="grid grid-cols-4 justify-center mx-auto gap-10  p-2 max-w-[1400px]">
+      <div className="movie-card-loader animate-pulse">
+        <div className="bg-gray-700 w-full h-72 rounded-md"></div>
+        <div className="mt-4">
+          <div className="h-6 bg-gray-700 rounded w-3/4 mb-2"></div>
+          <div className="flex items-center gap-2">
+            <div className="h-4 w-12 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-16 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-10 bg-gray-700 rounded"></div>
+          </div>
+        </div>
+      </div>
+      <div className="movie-card-loader animate-pulse">
+        <div className="bg-gray-700 w-full h-72 rounded-md"></div>
+        <div className="mt-4">
+          <div className="h-6 bg-gray-700 rounded w-3/4 mb-2"></div>
+          <div className="flex items-center gap-2">
+            <div className="h-4 w-12 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-16 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-10 bg-gray-700 rounded"></div>
+          </div>
+        </div>
+      </div>
+      <div className="movie-card-loader animate-pulse">
+        <div className="bg-gray-700 w-full h-72 rounded-md"></div>
+        <div className="mt-4">
+          <div className="h-6 bg-gray-700 rounded w-3/4 mb-2"></div>
+          <div className="flex items-center gap-2">
+            <div className="h-4 w-12 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-16 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-10 bg-gray-700 rounded"></div>
+          </div>
+        </div>
+      </div>
+      <div className="movie-card-loader animate-pulse">
+        <div className="bg-gray-700 w-full h-72 rounded-md"></div>
+        <div className="mt-4">
+          <div className="h-6 bg-gray-700 rounded w-3/4 mb-2"></div>
+          <div className="flex items-center gap-2">
+            <div className="h-4 w-12 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-16 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-10 bg-gray-700 rounded"></div>
+          </div>
+        </div>
+      </div>
+      <div className="movie-card-loader animate-pulse">
+        <div className="bg-gray-700 w-full h-72 rounded-md"></div>
+        <div className="mt-4">
+          <div className="h-6 bg-gray-700 rounded w-3/4 mb-2"></div>
+          <div className="flex items-center gap-2">
+            <div className="h-4 w-12 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-16 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-10 bg-gray-700 rounded"></div>
+          </div>
+        </div>
+      </div>
+      <div className="movie-card-loader animate-pulse">
+        <div className="bg-gray-700 w-full h-72 rounded-md"></div>
+        <div className="mt-4">
+          <div className="h-6 bg-gray-700 rounded w-3/4 mb-2"></div>
+          <div className="flex items-center gap-2">
+            <div className="h-4 w-12 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-16 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-10 bg-gray-700 rounded"></div>
+          </div>
+        </div>
+      </div>
+      <div className="movie-card-loader animate-pulse">
+        <div className="bg-gray-700 w-full h-72 rounded-md"></div>
+        <div className="mt-4">
+          <div className="h-6 bg-gray-700 rounded w-3/4 mb-2"></div>
+          <div className="flex items-center gap-2">
+            <div className="h-4 w-12 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-16 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-10 bg-gray-700 rounded"></div>
+          </div>
+        </div>
+      </div>
+      <div className="movie-card-loader animate-pulse">
+        <div className="bg-gray-700 w-full h-72 rounded-md"></div>
+        <div className="mt-4">
+          <div className="h-6 bg-gray-700 rounded w-3/4 mb-2"></div>
+          <div className="flex items-center gap-2">
+            <div className="h-4 w-12 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-16 bg-gray-700 rounded"></div>
+            <span>•</span>
+            <div className="h-4 w-10 bg-gray-700 rounded"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
