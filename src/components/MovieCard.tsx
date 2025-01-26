a3c74eea7a7e7917b82dcd839e991f1ad5d2b62f
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-26 19:19:55 +0500

    add MovieCard component to display individual movie details

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,41 @@
+import { updateSearch } from "../appwrite";
+import { Movie } from "./MoviesList";
+
+function MovieCard({
+  movie: {
+    title,
+    overview,
+    poster_path,
+    vote_average,
+    original_language,
+    release_date,
+  },
+}: {
+  movie: Movie;
+}) {
+  return (
+    <div className="movie-card text-whtie">
+      <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
+
+      <div className="mt-4">
+        <h3>{title}</h3>
+        <div className="content">
+          <div className="rating">
+            <img src="star.svg" alt="staricom" />
+            {vote_average && <p>{vote_average.toFixed(1)}</p>}
+          </div>
+          <span>•</span>
+
+          {original_language && (
+            <p className="text-white">{original_language}</p>
+          )}
+
+          <span>•</span>
+          <div className="year">{release_date?.split("-")[0]}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
