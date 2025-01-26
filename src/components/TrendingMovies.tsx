86603a14cdc912036ec20842e98ff5523f23bfee
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-26 19:20:03 +0500

    add TrendingMovies component to display trending movie list with loading state

---
+++ src/components/TrendingMovies.tsx
@@ -0,0 +1,113 @@
+import { useEffect, useState } from "react";
+import { getTrendingMovies } from "../appwrite";
+import { Models } from "appwrite";
+
+interface MovieData {
+  $collectionId: string;
+  $createdAt: string;
+  $databaseId: string;
+  $id: string;
+  $permissions?: string[]; // You can specify the type inside the array if you know its structure
+  $updatedAt: string;
+  count: number;
+  movie_id: number;
+  poster_url: string;
+  searchTerm: string;
+}
+
+function TrendingMovies() {
+  const [error, setError] = useState<string>("");
+  const [trendingMovies, setTrendingMovies] = useState<Models.Document[]>([]); // Specify the array type here
+  const [loading, setLoading] = useState(true);
+  const loadTrendingMovies = async () => {
+    try {
+      // Fetching the trending movies
+      const movies = await getTrendingMovies();
+      if (movies) {
+        setTrendingMovies(movies);
+        setLoading(false);
+      } else {
+        setError("Failed to load trending movies");
+        setLoading(false);
+      }
+      // Assuming the response is an array of `MovieData`
+
+      console.log(movies);
+    } catch (error: unknown) {
+      // Handle errors
+      if (error instanceof Error) {
+        console.log(error);
+        setError(error.message); // Now TypeScript knows about `message`
+      } else {
+        console.log("An unknown error occurred");
+        setError("An unknown error occurred");
+      }
+    }
+  };
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
+
+  if (loading) {
+    return <LoaderTrendingMovies />;
+  }
+  return (
+    <>
+      <section className="trending">
+        <h2>Trending Movies</h2>
+        <ul>
+          {trendingMovies.map((movie, index) => {
+            return (
+              <li key={movie.$id}>
+                <p>{index + 1}</p>
+                <img src={movie?.poster_url} />
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </>
+  );
+}
+
+export default TrendingMovies;
+
+const LoaderTrendingMovies = () => {
+  return (
+    <>
+      <div className="flex gap-2">
+        <div className="movie-card-loader animate-pulse">
+          <div className="bg-gray-700 w-full h-72 rounded-md"></div>
+          <div className="mt-4">
+            <div className="h-6 bg-gray-700 rounded w-3/4 mb-2"></div>
+          </div>
+        </div>
+        <div className="movie-card-loader animate-pulse">
+          <div className="bg-gray-700 w-full h-72 rounded-md"></div>
+          <div className="mt-4">
+            <div className="h-6 bg-gray-700 rounded w-3/4 mb-2"></div>
+          </div>
+        </div>
+        <div className="movie-card-loader animate-pulse">
+          <div className="bg-gray-700 w-full h-72 rounded-md"></div>
+          <div className="mt-4">
+            <div className="h-6 bg-gray-700 rounded w-3/4 mb-2"></div>
+          </div>
+        </div>
+        <div className="movie-card-loader animate-pulse">
+          <div className="bg-gray-700 w-full h-72 rounded-md"></div>
+          <div className="mt-4">
+            <div className="h-6 bg-gray-700 rounded w-3/4 mb-2"></div>
+          </div>
+        </div>
+        <div className="movie-card-loader animate-pulse">
+          <div className="bg-gray-700 w-full h-72 rounded-md"></div>
+          <div className="mt-4">
+            <div className="h-6 bg-gray-700 rounded w-3/4 mb-2"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
